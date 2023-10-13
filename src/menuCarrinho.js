32b112b8381f8b75b1f7e36046bb3248531d1ee8
WILLIAM <william.santosf@gmail.com>
2023-10-13 00:25:41 -0300

    CRIANDO A LÓGICA DE REMOÇÃO DO CARRINHO
    Nesse ponto, vamos criar a lógica que faz com que o produto saia do carrinho.
    A estratégia será, cada vez que um produto é removido do carrinho, a página deve acessar o dicionário e renderizar todo o carrinho novamente.
    Primeiro, criamos a função renderizarCarrinho, que cria um container com um HTML vazio, em seguida percorre o dicionário e, para cada item encontrado, executa a função desenharProdutoCarrinho.
    Depois, primeiro criamos a removerDoCarrinho, que recebe como parâmetro o idProduto, remove do dicionário e renderiza o carrinho novamente.
    Em seguida, aplicamos a função remover carrinho em dois lugares:
    1. dentro da função desenharProdutoCarrinho, onde adicionamos a funcionalidade ao botão que exclui o produto do carrinho, remover-item-${produto.id}
    2. dentro da função de decremento, onde através da condicional if, checamos se o valor no dicionário é igual a 1, quando o botão de decremento é clicado, e se verdadeira, aciona  a função removerDoCarrinho, que também é responsável por renderizar o carrinho novamente.

--- src/menuCarrinho.js
+++ src/menuCarrinho.js
@@ -20,12 +20,23 @@ export function inicializarCarrinho() {
   botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
 }
 
+
+//1. Primeiro criamos a função que DICIONÁRIO e exclui o produto do docionário
+function removerDoCarrinho (idProduto){                 // 5. função recebe como parâmetro o idProduto 
+    delete idsProdutoCarrinhoComQuantidade[idProduto];  // 5.1 acessa o docionário e remove o produto do dicionário pelo idProduto     
+    renderizarProdutosCarrinho();                       // 5.2 renderiza o carrinho novamente com base nos dados do dicionário.      
+}
+
 function incrementarQuantidadeProduto(idProduto) {
   idsProdutoCarrinhoComQuantidade[idProduto]++;
   atualizarInformacaoQuantidade(idProduto); 
 }
 
-function decrementarQuantidadeProduto(idProduto) {
+function decrementarQuantidadeProduto(idProduto) {   // 7. Agora atualizamos a função decrementar, que também será capaz de remover um produto do carrinho                
+    if (idsProdutoCarrinhoComQuantidade[idProduto] === 1){ // 7.1 A condicional checa se a quantidade do produto no dicionário é igual a 1.
+        removerDoCarrinho(idProduto);                // 7.2 Se a condição for verdadeira, a função remover carrinho será acionada.           
+        return;                                      // 7.3 e o fluxo retorna, ou seja, acaba. Caso contrário contonua a decrementar.                
+    }
   idsProdutoCarrinhoComQuantidade[idProduto]--;
   atualizarInformacaoQuantidade(idProduto); 
 }
@@ -35,12 +46,9 @@ function atualizarInformacaoQuantidade(idProduto) {
     idsProdutoCarrinhoComQuantidade[idProduto];
 }
 
-export function adicionarAoCarrinho(idProduto) {
-  if (idProduto in idsProdutoCarrinhoComQuantidade) { 
-    incrementarQuantidadeProduto(idProduto);
-    return; 
-  }
-  idsProdutoCarrinhoComQuantidade[idProduto] = 1; 
+//2. Criamos a função desenharProdutoCarrinho para que, após deleção, o os produtos sejam atualizados com base nas informações do dicionário.
+//Ou seja, o trecho de código desde o momento é encontrado no catálogo e colocado no carrinho, com todas as suas funcionalidades, agora passa a ser uma ,função separada.
+function desenharProdutoNoCarrinho (idProduto){
   const produto = catalogo.find((p) => p.id === idProduto);
   
   const containerProdutosCarrinho =
@@ -52,9 +60,9 @@ export function adicionarAoCarrinho(idProduto) {
   
   for (const articleClass of articleClasses){ 
     elementoArticle.classList.add(articleClass);
-  }
+  } //adicionado o id no botão que remove o produto do carrinho
     const cartaoProdutoCarrinho = `
-    <button id="fechar-carrinho" class="absolute top-0 right-2"><i 
+    <button id="remover-item-${produto.id}" class="absolute top-0 right-2"><i 
         class="fa-solid fa-circle-xmark text-slate-500 hover:text-slate-800"></i></button>
       <img src="./assets/img/${produto.nomeArquivoImagem}" alt="Carrinho: ${
     produto.nome
@@ -83,4 +91,28 @@ export function adicionarAoCarrinho(idProduto) {
     document
     .getElementById(`incrementar-produto-${idProduto}`)
     .addEventListener("click", () => incrementarQuantidadeProduto(produto.id));
+
+    document
+    .getElementById(`remover-item-${produto.id}`)                      //6.1 recuperamos o elemento do HTML, no caso o "X" que remove o botão                        
+    .addEventListener("click", () => removerDoCarrinho(produto.id));   //6.2 adicionamos o evento ao clicar no X que execura a função d eremover do carrinho                        
+}
+
+//4.A função renderizar produtos carrinho, será responsável por refazer o containerProdutosCarrinho, com base nas informações contidas no dicionário. Sendo:
+function renderizarProdutosCarrinho (){                      //4.1 criação da função renderizar carrinho  
+    const containerProdutosCarrinho=                         //4.2 criação da variável que acessar o HTML do container de produtos,
+    document.getElementById("produtos-carrinho");            //o produtos-carrinho
+    containerProdutosCarrinho.innerHTML = "";                //4.3 injetamos um HTML vazio na variável
+
+    for (const idProduto in idsProdutoCarrinhoComQuantidade){//4.4 criamos um loop for para percorrer o dicionário e 
+        desenharProdutoNoCarrinho(idProduto);                //executar a função de desenhar o produto para cada idProduto no dcionário.
+    }
+}
+
+export function adicionarAoCarrinho(idProduto) {
+  if (idProduto in idsProdutoCarrinhoComQuantidade) { 
+    incrementarQuantidadeProduto(idProduto);
+    return; 
+  }
+  idsProdutoCarrinhoComQuantidade[idProduto] = 1;
+  desenharProdutoNoCarrinho(idProduto); //3. Todo o trecho de código que estava aqui, agora é apenas uma função, que terá o mesmo efeito.
 }
